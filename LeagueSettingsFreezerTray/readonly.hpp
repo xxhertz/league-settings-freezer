c24fa8fa1622718e7c764a7860723d10328c471e
hz <47288761+xxhertz@users.noreply.github.com>
2024-03-17 00:36:52 -0400

    add set_readonly & bit helper function

--- LeagueSettingsFreezerTray/readonly.hpp
+++ LeagueSettingsFreezerTray/readonly.hpp
@@ -2,6 +2,11 @@
 #include <Windows.h>
 #include <string>
 
+// Unsets a bit, then sets it if `enable` is true
+inline DWORD set_bit(DWORD bitflag, DWORD flag, bool enable) {
+	return (bitflag & ~flag) | (enable ? flag : 0);
+}
+
 // Returns `true` if a file is readonly, and false otherwise
 inline bool is_readonly(std::string file) {
 	return GetFileAttributesA(file.c_str()) & FILE_ATTRIBUTE_READONLY;
@@ -10,4 +15,9 @@ inline bool is_readonly(std::string file) {
 // Toggles a file's readonly state
 inline void toggle_readonly(std::string file) {
 	SetFileAttributesA(file.c_str(), GetFileAttributesA(file.c_str()) ^ FILE_ATTRIBUTE_READONLY);
+}
+
+// Changes a file's readonly state
+inline void set_readonly(std::string file, bool readonly) {
+	SetFileAttributesA(file.c_str(), set_bit(GetFileAttributesA(file.c_str()), FILE_ATTRIBUTE_READONLY, readonly));
 }
